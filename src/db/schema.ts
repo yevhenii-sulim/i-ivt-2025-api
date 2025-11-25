d52571d67e829b6fa282eec3144de0425e6a61f3
yevhenii-sulim <bees127@ukr.net>
2025-11-25 16:35:52 +0200

    added schema for user

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,2 +1,9 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
-export const usersTable = pgTable('user', {});
+import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core';
+export const usersTable = pgTable('user', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  firstName: varchar('firstname', { length: 255 }).notNull(),
+  lastName: varchar('lastname', { length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+});
