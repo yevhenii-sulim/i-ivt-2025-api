ce60eb004f1aacb930dd96ee65c57255e769e265
yevhenii-sulim <bees127@ukr.net>
2025-11-27 19:09:44 +0200

    finish api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { DrizzleService } from '~/drizzle/drizzle.service';
+import { AuthGuard } from '~/guards/auth.guard';
 import { UserController } from '~/user/user.controller';
 import { UserService } from '~/user/user.service';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService, DrizzleService],
+  providers: [UserService, DrizzleService, AuthGuard],
   exports: [UserService],
 })
 export class UserModule {}
