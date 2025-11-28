f277911f7d18d23a997de667cf6e67055394ab46
yevhenii-sulim <bees127@ukr.net>
2025-11-28 12:13:10 +0200

    add prefix global

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Uer API')
@@ -11,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
